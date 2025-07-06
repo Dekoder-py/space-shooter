76e12440a62d751c4f7e6dec3f0447c0f1a3eb13
Dekoder-py <dekod3r.py@gmail.com>
2025-07-07 11:46:43 +1200

    add surface

--- code/main.py
+++ code/main.py
@@ -11,6 +11,9 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 # set the title of the window
 pygame.display.set_caption('Space Shooter')
 
+# surface
+surf = pygame.Surface((100, 200))
+
 while running:
     # event loop
     for event in pygame.event.get():
@@ -19,6 +22,7 @@ while running:
 
     # draw the game
     display_surface.fill('blue')
+    display_surface.blit(surf, (100, 150))
     pygame.display.update()
 
 pygame.quit()
