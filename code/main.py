1f78db6fced42376b953a7c8a098dc47efab240c
Dekoder-py <dekod3r.py@gmail.com>
2025-07-07 18:22:53 +1200

    add check for player off screen, add meteor

--- code/main.py
+++ code/main.py
@@ -20,13 +20,19 @@ surf = pygame.Surface((100, 200))
 surf.fill('orange')
 x = 100
 
-# import an image for the player
+# imports
+
+# player
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
 player_rect = player_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 
-# import star
+# star
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
+
+# meteor
+meteor_surf = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
+meteor_rect = meteor_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 
 while running:
     # event loop
@@ -38,6 +44,11 @@ while running:
     display_surface.fill('darkgrey')
     for i in range(20):
         display_surface.blit(star_surf, star_positions[i])
+    if player_rect.right < WINDOW_WIDTH:
+        player_rect.left += 0.2
+
+    display_surface.blit(meteor_surf, meteor_rect)
+
     display_surface.blit(player_surf, player_rect)
     pygame.display.update()
 
